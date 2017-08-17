ad200b16b1698cc149c1704427fc95bdd8d9bc6b
annaprokopchuk <prokopchuk180493@gmail.com>
2017-08-17 18:51:35 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  if ($('.slider1').length) {
+    $('.slider1').slick({
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 2000
+      // appendArrows: ".appendArrows",
+    });
+   }
+})
+
+$(document).ready(function () {
+  if ($('.slider-brands').length) {
+    $('.slider-brands').slick({
+      infinite: true,
+      // autoplay: true,
+      autoplaySpeed: 2000,
+      slidesToShow: 6,
+      slidesToScroll: 1
+    });
+  }
+})
+
+jQuery(document).ready(function($) {
+    $('.instagram-container').masonry({
+        // options
+        itemSelector: '.card-instagram',
+        columnWidth: '.persent-size',
+        percentPosition: true
+    });
+});
